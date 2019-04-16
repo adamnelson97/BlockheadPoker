0b89c39e54904588b4fb1b64582c35cdd7ec985d
Adam Nelson <adamnelson@mines.edu>
2019-04-16 15:16:52 -0600

    Completed Player class

--- cmake-build-debug/player.cpp
+++ cmake-build-debug/player.cpp
@@ -1,5 +1,60 @@
+//========================================================
 //
-// Created by adamn on 4/16/2019.
+//  File Name:   player.cpp
 //
+//  Author:  Adam Nelson
+//
+//  Course and Assignment:   CSCI 477, Project 2 - Phase 1
+//
+//  Description:  The definition file for the player class
+//
+// =========================================================
 
+//-----------------------------------------------------------
+// Name: Player
+//
+// Description: This represents either a human or AI player.
+//      This is a base abstract class. You then should have
+//      derived classes for human, Alpha AI players, and Beta
+//      AI players (or more). The human player class will
+//      basically be I/O functions that presents info to the
+//      game player and gest decisions from a game player.
+//      The AI classes should make their own decisions without
+//      human input.
+//
+// -----------------------------------------------------------
+
+#include "bethistory.h"
+#include "bet.h"
+#include "hand.h"
 #include "player.h"
+using namespace std;
+
+Player::Player(int id, int chips) {
+    playerID = id;
+    playerChips = chips;
+}
+
+int Player::getID() {
+    return playerID;
+}
+
+void Player::clearHand() {
+    playerHand.clear();
+}
+
+void Player::dealCard(Card c) {
+    playerHand.push_back(c);
+}
+
+Hand Player::getHand() {
+    return playerHand.getVisible();
+}
+
+void Player::addChips(int chips) {
+    playerChips += chips;
+}
+
+int Player::getChips() {
+    return playerChips;
+}
--- cmake-build-debug/player.h
+++ cmake-build-debug/player.h
@@ -13,7 +13,37 @@
 //-----------------------------------------------------------
 // Name: Player
 //
-// Description: The BetHistory contains all log of all bets
-//      in a single round.
+// Description: This represents either a human or AI player.
+//      This is a base abstract class. You then should have
+//      derived classes for human, Alpha AI players, and Beta
+//      AI players (or more). The human player class will
+//      basically be I/O functions that presents info to the
+//      game player and gest decisions from a game player.
+//      The AI classes should make their own decisions without
+//      human input.
 //
 // -----------------------------------------------------------
+
+#pragma once
+#include "bethistory.h"
+#include "bet.h"
+#include "hand.h"
+using namespace std;
+
+class Player {
+public:
+    Player(int id, int chips);
+    virtual int getBet(Hand opponent, BetHistory bh,
+            int bet2player, bool canRaise, int pot);
+    int getID();
+    void clearHand();
+    void dealCard(Card c);
+    Hand getHand();
+    void addChips(int chips);
+    int getChips();
+
+private:
+    int playerID;
+    int playerChips;
+    Hand playerHand;
+};
