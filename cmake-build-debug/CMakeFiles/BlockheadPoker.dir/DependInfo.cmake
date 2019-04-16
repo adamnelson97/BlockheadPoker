30066d25b402eae75976ff1427b6397978185a29
Adam Nelson <[adamnelson@mines.edu]>
2019-04-16 16:15:54 -0600

    Added files for creating executable

---
+++ cmake-build-debug/CMakeFiles/BlockheadPoker.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "Z:/adit/My Documents/BlockheadPoker/cmake-build-debug/alphaplayer.cpp" "Z:/adit/My Documents/BlockheadPoker/cmake-build-debug/CMakeFiles/BlockheadPoker.dir/alphaplayer.cpp.obj"
+  "Z:/adit/My Documents/BlockheadPoker/cmake-build-debug/bet.cpp" "Z:/adit/My Documents/BlockheadPoker/cmake-build-debug/CMakeFiles/BlockheadPoker.dir/bet.cpp.obj"
+  "Z:/adit/My Documents/BlockheadPoker/cmake-build-debug/bethistory.cpp" "Z:/adit/My Documents/BlockheadPoker/cmake-build-debug/CMakeFiles/BlockheadPoker.dir/bethistory.cpp.obj"
+  "Z:/adit/My Documents/BlockheadPoker/cmake-build-debug/card.cpp" "Z:/adit/My Documents/BlockheadPoker/cmake-build-debug/CMakeFiles/BlockheadPoker.dir/card.cpp.obj"
+  "Z:/adit/My Documents/BlockheadPoker/cmake-build-debug/game.cpp" "Z:/adit/My Documents/BlockheadPoker/cmake-build-debug/CMakeFiles/BlockheadPoker.dir/game.cpp.obj"
+  "Z:/adit/My Documents/BlockheadPoker/cmake-build-debug/hand.cpp" "Z:/adit/My Documents/BlockheadPoker/cmake-build-debug/CMakeFiles/BlockheadPoker.dir/hand.cpp.obj"
+  "Z:/adit/My Documents/BlockheadPoker/cmake-build-debug/humanplayer.cpp" "Z:/adit/My Documents/BlockheadPoker/cmake-build-debug/CMakeFiles/BlockheadPoker.dir/humanplayer.cpp.obj"
+  "Z:/adit/My Documents/BlockheadPoker/cmake-build-debug/main.cpp" "Z:/adit/My Documents/BlockheadPoker/cmake-build-debug/CMakeFiles/BlockheadPoker.dir/main.cpp.obj"
+  "Z:/adit/My Documents/BlockheadPoker/cmake-build-debug/player.cpp" "Z:/adit/My Documents/BlockheadPoker/cmake-build-debug/CMakeFiles/BlockheadPoker.dir/player.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/BlockheadPoker.dir/cmake_clean.cmake
@@ -0,0 +1,20 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/BlockheadPoker.dir/main.cpp.obj"
+  "CMakeFiles/BlockheadPoker.dir/card.cpp.obj"
+  "CMakeFiles/BlockheadPoker.dir/bet.cpp.obj"
+  "CMakeFiles/BlockheadPoker.dir/game.cpp.obj"
+  "CMakeFiles/BlockheadPoker.dir/hand.cpp.obj"
+  "CMakeFiles/BlockheadPoker.dir/bethistory.cpp.obj"
+  "CMakeFiles/BlockheadPoker.dir/player.cpp.obj"
+  "CMakeFiles/BlockheadPoker.dir/humanplayer.cpp.obj"
+  "CMakeFiles/BlockheadPoker.dir/alphaplayer.cpp.obj"
+  "BlockheadPoker.pdb"
+  "BlockheadPoker.exe"
+  "BlockheadPoker.exe.manifest"
+  "libBlockheadPoker.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/BlockheadPoker.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
